da4440354938175de3efa512c8d2a81aaa860583
James-Millan <nf20792@bristol.ac.uk>
2022-12-06 17:54:57 +0000

    2d working

--- scripts/bruteForceNonceRespecting.py
+++ scripts/bruteForceNonceRespecting.py
@@ -4,29 +4,37 @@ import itertools
 current_answer = ""
 answer_found = False
 correct_plaintext = ""
-initial_plaintext = "It is one of the blessings of old friends that you can afford to be stupid with "
+initial_plaintext = "The enemy knows "
 message = initial_plaintext
-# alphabet = "them."
+ciphertext = "1a3d3c5d67daf2707ef74e38478d7fbf0c7648fae3e29264b147bfc5fdf00205"
+c_block1 = "1a3d3c5d67daf2707ef74e38478d7fbf"
+c_block2 = "0c7648fae3e29264b147bfc5fdf00205"
+output = "ea486e17628aef1d009930c1b91f3ea41881692d1ee52ed63e73d70e1a8d00434fc2313a8e691c43046dc749153f08b7"
 alphabet = "abcdefghijklmnopqrstuvwxyz "
 message = initial_plaintext
 nonce = ""
-with open("../publics/n1_cbc.txt") as f:
+with open("../publics/n2_cbc.txt") as f:
     nonce = f.readline().strip()
 
-for k in range(6):
+
+expected_value = subprocess.run(["../cbc", c_block2, c_block1 ], stdout=subprocess.PIPE).stdout.decode()
+print(expected_value)
+
+for k in range(16):
     subsets = itertools.permutations(alphabet, k)
     if answer_found:
         break
     for v in subsets:
-        message = initial_plaintext
+        message = ""
         #print(v)
         if answer_found:
             break
         for i in v:
             message = message + i
-        current_answer = subprocess.run(["../cbc", nonce, message], stdout=subprocess.PIPE).stdout.decode()
-        if correct_ciphertext == current_answer:
-            correct_plaintext = message
+            message = message + "."
+        current_answer = subprocess.run(["../cbc", message, nonce], stdout=subprocess.PIPE).stdout.decode()
+        if output == current_answer:
+            correct_plaintext = initial_plaintext + message
             answer_found = True
             print("WE HAVE CRACKED IT!\n"*5)
             break
@@ -48,4 +56,4 @@ print(correct_plaintext)
 #     print(subprocess.run(["../cbc", nonce, message]))
 
 
-
+6a504d5b09959d3137bea0fd93b1f4b99fc884e8cd7a3
--- scripts/meetingInTheMiddle.py
+++ scripts/meetingInTheMiddle.py
@@ -1,27 +1,30 @@
 from Crypto.Cipher import AES
 
+message = bytes.fromhex("0123456789abcdef0123456789abcdef")
+ciphertext = bytes.fromhex("1a7f3e8d067c8a281cac681b815333a7")
+print(ciphertext)
 
-message = "22222222222222222222222222222222"
-ciphertext = "c35129a775ddcf8396fade0573bccfb7"
+print(aes1.encrypt(message) == aes2.decrypt(ciphertext))
+c1_list = set()
+k1_list = {}
+k2_list = {}
+c2_list = set()
+for i in range(0, 65537):
+    hexKey = i.to_bytes(32, 'big')
+    aesE = AES.new(hexKey, AES.MODE_ECB)
+    k1_cipher = aesE.encrypt(message)
+    c1_list.add(k1_cipher)
+    k1_list[k1_cipher] = i
 
-key = "password"
-aes = AES.new(key, AES.MODE_EBC)
-
-k1_list = []
-k2_list = []
-for i in range(65537):
-    aes = AES.new(i, AES.MODE_EBC)
-    k1_cipher = aes.encrypt(message)
-    k1_list.append(k1_cipher)
-
-for j in range(65537):
-    aes = AES.new(j, AES.MODE_EBC)
-    k2_cipher = aes.decrypt(ciphertext)
-    k2_list.append(k2_cipher)
-    if k2_cipher in k1_list:
-        candidate_key_1 = k1_list[k2_cipher].index()
+for j in range(0, 65537):
+    hexKey = j.to_bytes(32, 'big')
+    aesD = AES.new(hexKey, AES.MODE_ECB)
+    k2_cipher = aesD.decrypt(ciphertext)
+    c2_list.add(k2_cipher)
+    k2_list[k2_cipher] = hexKey
+    if k2_cipher in c1_list:
+        print("FOUND "*5)
+        candidate_key_1 = k1_list[k2_cipher]
         candidate_key_2 = j
-        aes1 = AES.new(candidate_key_1, AES.MODE_EBC)
-        if aes.encrypt(aes1.encrypt(message)) == ciphertext:
-            print("key 1 is:- " + str(candidate_key_1))
-            print("key 2 is:- " + str(candidate_key_2))
+        print("key 1 is:- " + str(candidate_key_1))
+        print("key 2 is:- " + str(candidate_key_2))
